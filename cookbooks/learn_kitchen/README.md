496920290a6d6c84b010376cd1b11a5a02d50183
ganeshp <ganeshp@thoughtworks.com>
2016-10-14 00:16:05 +0530

    generated new chef cookbook to learn basic for kitchen

---
+++ cookbooks/learn_kitchen/README.md
@@ -0,0 +1,4 @@
+# learn_kitchen
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/learn_kitchen/metadata.rb
@@ -0,0 +1,15 @@
+name 'learn_kitchen'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures learn_kitchen'
+long_description 'Installs/Configures learn_kitchen'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/learn_kitchen/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/learn_kitchen' if respond_to?(:source_url)
